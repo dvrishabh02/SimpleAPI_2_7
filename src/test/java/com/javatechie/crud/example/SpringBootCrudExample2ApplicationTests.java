cec770a52d2d3428a073c06c67a5daee716247e3
dvrishabh02 <devrishabhsharma02@gmail.com>
2024-09-07 01:10:22 +0530

    changes after migration

--- src/test/java/com/javatechie/crud/example/SpringBootCrudExample2ApplicationTests.java
+++ src/test/java/com/javatechie/crud/example/SpringBootCrudExample2ApplicationTests.java
@@ -3,7 +3,7 @@ package com.javatechie.crud.example;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,classes = SpringBootCrudExample2Application.class)
 class SpringBootCrudExample2ApplicationTests {
 
 	@Test
